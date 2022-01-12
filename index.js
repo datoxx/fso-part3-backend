e668edbdad98dcb7c2079e47a548a56e6ec23f00
datoxx <davit.ardzenadze.05@gmail.com>
2022-01-13 02:23:35 +0400

    3.13-3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
+const PhoneNote = require('./models/number')
 
 const app = express()
 
@@ -14,62 +16,29 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let phoneBook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    res.json(phoneBook)
+    PhoneNote.find({})
+    .then(numbers => {
+        res.json(numbers)
+    })
 })
 
-const generateId = () => {
-    const id = Math.floor(Math.random() * 10000000);
-    return id;
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'content missing' })
   }
 
-
-  app.post('/api/persons', (req, res) => {
-    const body = req.body
-    const sameName = phoneBook.some(n => n.name === body.name)
-
-    if(!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'name or number missing'
-        })
-    } else if(sameName) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const phone = {
-        id : generateId(),
+    const phone = new PhoneNote({
         name: body.name,
         number: body.number
-    }
+    })
 
-    phoneBook = phoneBook.concat(phone)
-
-    res.json(phone)
+    phone.save()
+    .then(newNumber => {
+        res.json(newNumber)
+    })
 })
 
 app.get('/info', (req, res) => {
@@ -77,14 +46,10 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const phone = phoneBook.find(n => n.id === id)
-
-    if(phone) {
-        res.json(phone)
-    } else {
-        res.status(404).end()
-    }
+    PhoneNote.findById(req.params.id)
+    .then(number => {
+        res.json(number)
+    })
 
 })
 
@@ -96,7 +61,7 @@ app.delete('/api/persons/:id', (req, res) => {
   })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
