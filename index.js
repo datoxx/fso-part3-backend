9ab3b477d102979f9e1160f7ba815a083d8d5ddf
datoxx <davit.ardzenadze.05@gmail.com>
2022-01-18 17:26:09 +0400

    3.22 display Lint configuration

--- index.js
+++ index.js
@@ -1,87 +1,88 @@
-require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors')
-const PhoneNote = require('./models/number');
-const { request } = require('express');
-
+/* eslint-disable  */
+const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const PhoneNote = require('./models/number')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
 const errorHandle = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if(error.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if(error.name === 'CastError') {
+    return res.status(400).send({error: 'malformation id'})
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
-morgan.token('body', (req) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body))
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
-    PhoneNote.find({})
+  PhoneNote.find({})
     .then(numbers => {
-        res.json(numbers)
+      res.json(numbers)
     })
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
   if (body.name === undefined) {
     return res.status(400).json({ error: 'content missing' })
   }
 
-    const phone = new PhoneNote({
-        name: body.name,
-        number: body.number
-    })
+  const phone = new PhoneNote({
+    name: body.name,
+    number: body.number
+  })
 
-    phone.save()
+  phone.save()
     .then(newNumber => {
-        res.json(newNumber)
+      res.json(newNumber)
     })
     .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-    res.send(`<h1>Phonebook has info ${phoneBook.length} people </h1> <p>${new Date}</p>`)
+  // eslint-disable-next-line no-undef
+  res.send(`<h1>Phonebook has info ${phoneBook.length} people </h1> <p>${new Date}</p>`)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    PhoneNote.findById(req.params.id)
+  PhoneNote.findById(req.params.id)
     .then(number => {
-        res.json(number)
+      res.json(number)
     })
 
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    PhoneNote.findByIdAndRemove(req.params.id)
-    .then(result => {
-        res.status(204).end()
+  PhoneNote.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
     })
     .catch(error => next(error))
-  })
+})
 
-  const unknownEndpoint = (req, res) => {
-      res.status(404).send({error: 'unknown endpoint'})
-  }
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
 
-  app.use(unknownEndpoint)
+app.use(unknownEndpoint)
 
-  app.use( errorHandle)
+app.use( errorHandle)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
